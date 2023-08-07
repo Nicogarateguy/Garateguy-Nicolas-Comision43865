f68d29a79002a186f5870dd3ae2957f62718bc3a
NicolasGarateguy <dngarateguy@gmail.com>
2023-08-07 18:32:13 -0300

    version 8

--- app/admin.py
+++ app/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import Carrera, Postgrado, Docente, Alumno
 
 # Register your models here.
+admin.site.register(Carrera)
+admin.site.register(Postgrado)
+admin.site.register(Docente)
+admin.site.register(Alumno)
--- app/models.py
+++ app/models.py
@@ -7,6 +7,9 @@ class Carrera(models.Model):
     modalidad = models.CharField(max_length=20)
     duracion = models.TextField(max_length=20)
     financiacion = models.TextField()
+
+    def __str__(self):
+        return f"{self.nombre} "
     
 
 class Postgrado(models.Model):
@@ -16,6 +19,9 @@ class Postgrado(models.Model):
     duracion = models.TextField(max_length=20)
     financiacion = models.TextField()
 
+    def __str__(self):
+        return f"{self.nombre} "
+
  
 class Docente(models.Model):
 
@@ -23,6 +29,9 @@ class Docente(models.Model):
     apellido = models.CharField(max_length=50)
     email = models.EmailField()
     profesion = models.CharField(max_length=50)
+
+    def __str__(self):
+        return f"{self.apellido}, {self.nombre} "
 
 
 class Alumno(models.Model):
